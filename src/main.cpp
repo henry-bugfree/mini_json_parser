34190477669406efa0a2b8469fa29839429d6872
henry-bugfree <71884274+henry-bugfree@users.noreply.github.com>
2020-11-09 17:01:43 +0800

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "json_scanner.h"
+#include "json_parser.h"
+using namespace std;
+
+extern vector<string> text;
+
+int main() {
+
+    scanner("../json.txt");
+    parser();
+    return 0;
+}
+
